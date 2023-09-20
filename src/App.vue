be14bd83a2e57037347ec60267ff320e48fdbca1
Rea Lyn G. Cuadrante <cuadranterealyn68@gmail.com>
2023-09-20 23:15:43 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="BSIT 4-4 CUADRANTE/CRESIDIO/HAGOSOJOS/GERNALE" />
 </template>
 
 <style scoped>

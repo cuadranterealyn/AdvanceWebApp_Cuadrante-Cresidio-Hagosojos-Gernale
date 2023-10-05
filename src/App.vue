0af0606db298a895d0f4abde38cef4a7520641b2
Rea Lyn G. Cuadrante <cuadranterealyn68@gmail.com>
2023-10-05 16:14:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const input = ref('')
+</script>
 <template>
-  
+    <!-- reactivity -->
+    <!-- two-way binding -->
+    <!-- directives (v-model) -->
+    <select v-model="input">
+        <option value="web">web</option>
+        <option value="net">net</option>
+        <option value="others">others</option>
+    </select>
+    <p>p: {{ input }}</p>
+    <!-- conditional -->
+    <p v-if="input == 'web'">
+    <p>Web Development</p>
+    </p>
+    <p v-else-if="input == 'net'">Networking</p>
+    <p v-else>Others</p>
+
+    <p v-if="input == 'web'">
+    <p>Web Development 2.0</p>
+    </p>
+    <!-- commit and push -->
 </template>

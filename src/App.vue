9c53b3fefb6046e6ca675a6fbc448b724372ab69
Rea Lyn Cuadrante <cuadranterealyn68@gmail.com>
2023-11-10 19:24:16 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import BaseCondition from './components/BaseCondition.vue'
+import BaseLoop from './components/BaseLoop.vue'
 
-const input = ref('')
 </script>
-<template>
-    <!-- reactivity -->
-    <!-- two-way binding -->
-    <!-- directives (v-model) -->
-    <select v-model="input">
-        <option value="web">web</option>
-        <option value="net">net</option>
-        <option value="others">others</option>
-    </select>
-    <p>p: {{ input }}</p>
-    <!-- conditional -->
-    <p v-if="input == 'web'">
-    <p>Web Development</p>
-    </p>
-    <p v-else-if="input == 'net'">Networking</p>
-    <p v-else>Others</p>
 
-    <p v-if="input == 'web'">
-    <p>Web Development 2.0</p>
-    </p>
-    <!-- commit and push -->
+<template>
+    <div>
+        <BaseCondition/>
+      </div>
+      <div>
+            <BaseLoop/>
+          </div>
 </template>
---
+++ src/components/BaseLoop.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+// Data property
+const strings = ref(['Cuadrante', 'Cresidio', 'Gernale', 'Hagosojos']);
+let loopIndex = ref(0);
+const showBSITText = ref(false);
+
+// Method to continue the loop
+const continueLoop = () => {
+    strings.value.push("BSIT Block-4 Students");
+};
+</script>
+
+<template>
+    <div>
+        <ul>
+            <!-- Example: Using v-for to loop through name -->
+            <li v-for="name in strings " :key=" name" style="font-style: italic;">{{ name  }}</li>
+        </ul>
+        <button @click="continueLoop">Continue Loop</button>
+       
+    </div>
+
+</template>
